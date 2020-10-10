6d796939d6410fe8d98fe80f91ed97a10ba92321
Josef Macků <josef@macku.cz>
2020-10-10 15:09:42 +0200

    process.env vars changed

--- util/mailing.js
+++ util/mailing.js
@@ -2,13 +2,13 @@ const nodemailer = require('nodemailer');
 const smtpTransport = require('nodemailer-smtp-transport');
 
 const transporter = nodemailer.createTransport(smtpTransport({
-    host: process.env.MAIL.HOST,
+    /* host: process.env.MAIL.HOST,
     port: process.env.MAIL.PORT,
     secure: true, // use SSL
     auth: {
         user: process.env.MAIL.USER,
         pass: process.env.MAIL.PASSWORD
-    }
+    } */
 }));
 
 module.exports = transporter;
