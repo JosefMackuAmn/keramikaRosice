50eee72fb39d982b3f6a9bd736de4c4824c2255b
Josef Macků <josef@macku.cz>
2020-12-05 11:54:14 +0100

    Fixed html entities in invoice

--- util/helpers.js
+++ util/helpers.js
@@ -30,3 +30,19 @@ exports.getDelPayLabel = (deliveryMethod, paymentMethod) => {
 exports.formatDate = (date) => {
     return date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear();
 }
+
+exports.replaceHtmlEntities = (string) => {
+    let replacedString = string.replace('&#x2f;', '/');
+    replacedString = replacedString.replace('&#x2F;', '/');
+    replacedString = replacedString.replace('&#x2c;', ',');
+    replacedString = replacedString.replace('&#x2C;', ',');
+    replacedString = replacedString.replace('&#x2d;', '-');
+    replacedString = replacedString.replace('&#x2D;', '-');
+    replacedString = replacedString.replace('&#x28;', '(');
+    replacedString = replacedString.replace('&#x29;', ')');
+    replacedString = replacedString.replace('&#x3a;', ':');
+    replacedString = replacedString.replace('&#x3A;', ':');
+    replacedString = replacedString.replace('&#x27;', '\'');
+
+    return replacedString;
+}
